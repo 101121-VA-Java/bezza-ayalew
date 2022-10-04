2bb64d249ae4632f55d7c4dd74fa0c3aac94cbe8
btayalew <bayalew@gmail.com>
2022-10-03 22:50:53 -0500

    Creating mager archiver

--- mager-avi-archiver/src/app/app.component.ts
+++ mager-avi-archiver/src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormioAuthService } from '@formio/angular/auth';
+import { FormioResources } from '@formio/angular/resource';
 
 @Component({
   selector: 'app-root',
-  template: '<formio src="https://clxseyaefwqpgaq.form.io/magerarchiver"></formio>',
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
+  // template: '<formio src="https://clxseyaefwqpgaq.form.io/magerarchiver"></formio>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'mager-avi-archiver';
+
+  constructor(
+    public auth: FormioAuthService,
+    private router: Router,
+    public resources: FormioResources
+  ) {
+    this.auth.onLogin.subscribe(() => {
+      this.router.navigate(['/']);
+    });
+
+    this.auth.onLogout.subscribe(() => {
+      this.router.navigate(['/auth/login']);
+    });
+
+    this.auth.onRegister.subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
