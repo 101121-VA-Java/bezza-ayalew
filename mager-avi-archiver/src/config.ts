963826fbceafc984859fe35aac8aaf349185353c
btayalew <bayalew@gmail.com>
2022-10-21 12:09:05 -0500

    Front- and back -end integration

--- mager-avi-archiver/src/config.ts
+++
@@ -1,6 +0,0 @@
-import { FormioAppConfig } from "angular-formio";
-
-export const AppConfig: FormioAppConfig = {
-    appUrl: 'https://clxseyaefwqpgaq.form.io',
-    apiUrl: 'https://api.form.io'
-  };
